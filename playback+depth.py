0ef91ee51aaf9fabb466ad705e877a54673703ae
Élisson Michael <elissonmichael@yahoo.com.br>
2013-06-01 17:00:23 -0300

    Exibir video e profundidade de um arquivo .oni

---
+++ playback+depth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from openni import *
+import cv2.cv as cv
+
+depth_cv = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 1)
+imagem_cv = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
+
+ni = Context()
+ni.init()
+ni.open_file_recording("SeuVideoAqui.oni")
+ni.start_generating_all()
+video = ni.find_existing_node(NODE_TYPE_IMAGE)
+depth = ni.find_existing_node(NODE_TYPE_DEPTH)
+
+def processa_frame(imagem):
+    cv.SetData(imagem_cv, imagem)
+    cv.ShowImage('Video', imagem_cv)
+
+def processa_profundidade(imagem):
+    cv.SetData(depth_cv, imagem)
+    cv.ShowImage('Profundidade', depth_cv)
+
+tecla = -1
+while (tecla < 0):
+    ni.wait_one_update_all(video)
+    imagem = video.get_raw_image_map_bgr()
+    depth_frame = depth.get_raw_depth_map_8()
+
+    processa_frame(imagem)
+    processa_profundidade(depth_frame)
+
+    tecla = cv.WaitKey(1)
+cv.DestroyAllWindows()
+
+
