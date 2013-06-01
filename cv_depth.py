7186dc9b54b90e0504346c51dc97c5d1e0268b30
Élisson Michael <elissonmichael@yahoo.com.br>
2013-06-01 15:26:17 -0300

    Descobri que não é necessário o Array(Como no Pykinect)

--- cv_depth.py
+++ cv_depth.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 from openni import *
 import cv2.cv as cv
-import array
 
 depth_cv = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 1)
-matriz = array.array('B')
 
 ctx = Context()
 ctx.init()
@@ -15,16 +13,15 @@ depth.set_resolution_preset(RES_VGA)
 depth.fps = 30
 ctx.start_generating_all()
 
-def processa_profundidade(imagem):
-    matriz.fromstring(imagem)
-    cv.SetData(depth_cv, imagem)
+def processa_profundidade(depth_frame):
+    cv.SetData(depth_cv, depth_frame)
     cv.ShowImage('Profundidade', depth_cv)
-
 
 tecla = -1
 while (tecla < 0):
     nRetVal = ctx.wait_one_update_all(depth)
-    processa_profundidade(depth.get_raw_depth_map_8())
+    depth_frame = depth.get_raw_depth_map_8()
+    processa_profundidade(depth_frame)
     tecla = cv.WaitKey(1)
 
 cv.DestroyAllWindows()
--- video_kinect.py
+++ video_kinect.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 from openni import *
 import cv2.cv as cv
-import array
 
 imagem_cv = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
-matriz = array.array('B')
 
 ctx = Context()
 ctx.init()
@@ -16,15 +14,15 @@ video.fps = 30
 
 ctx.start_generating_all()
 
-def processa_video(imagem):
-    matriz.fromstring(imagem)
-    cv.SetData(imagem_cv, imagem)
+def processa_frame(frame):
+    cv.SetData(imagem_cv, frame)
     cv.ShowImage('Video', imagem_cv)
 
 tecla = -1
 while (tecla < 0):
     nRetVal = ctx.wait_one_update_all(video)
-    processa_video(video.get_raw_image_map_bgr())
+    frame = video.get_raw_image_map_bgr()
+    processa_frame(frame)
     tecla = cv.WaitKey(1)
 
 cv.DestroyAllWindows()
