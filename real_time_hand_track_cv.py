408d80dd905e7cad70b8aaea00afc47ae1a58980
Élisson Michael <elissonmichael@yahoo.com.br>
2013-06-03 15:01:07 -0300

    Captura da mão sem ser por vídeo.

--- real_time_hand_track_cv.py
+++ real_time_hand_track_cv.py
@@ -22,11 +22,41 @@ depth = DepthGenerator()
 depth.create(ctx)
 depth.set_resolution_preset(RES_VGA)
 depth.fps = 30
+
+depth.alternative_view_point_cap.set_view_point(video)
+
+gesture_generator = GestureGenerator()
+gesture_generator.create(ni)
+gesture_generator.add_gesture('Wave')
+centro = (0,0)
+
+hands_generator = HandsGenerator()
+hands_generator.create(ctx)
+
 ctx.start_generating_all()
 
-def processa_video(imagem):
+def gesture_detected(src, gesture, id, end_point):
+    hands_generator.start_tracking(end_point)
+
+def gesture_progress(src, gesture, point, progress): pass
+
+def create(src, id, pos, time):
+    return 
+
+def update(src, id, pos, time):
+    global centro
+    centro = (int(pos[0]), int(pos[1]))
+
+def destroy(src, id, time):
+    return
+
+gesture_generator.register_gesture_cb(gesture_detected, gesture_progress)
+hands_generator.register_hand_cb(create, update, destroy)
+
+def processa_frame(imagem):
     cv.SetData(imagem_cv, imagem)
     cv.Flip(imagem_cv,imagem_cv,1)
+    cv.Circle(imagem_cv, centro, 16, cv.CV_RGB(0, 0, 255), 2, cv.CV_AA, 0)
     cv.ShowImage('Video', imagem_cv)
 
 def processa_profundidade(imagem):
@@ -36,9 +66,11 @@ def processa_profundidade(imagem):
 
 tecla = -1
 while (tecla < 0):
-    nRetVal = ctx.wait_one_update_all(video)
+    nRetVal = ctx.wait_any_update_all
     processa_video(video.get_synced_image_map_bgr())
     processa_profundidade(depth.get_raw_depth_map_8())
-    tecla = cv.WaitKey(1)
 
+    tecla = cv.WaitKey(1)
 cv.DestroyAllWindows()
+
+
