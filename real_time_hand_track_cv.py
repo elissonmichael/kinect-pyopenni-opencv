288e1a5d1625f79bf179c1552f260fcd0b0255d2
Élisson Michael <elissonmichael@yahoo.com.br>
2013-06-05 11:23:01 -0300

    Aviso para Acenar

--- real_time_hand_track_cv.py
+++ real_time_hand_track_cv.py
@@ -32,6 +32,7 @@ gesture_generator.add_gesture('Wave')
 centro = (0,0)
 coordenada_real = ''
 coordenada_projecao = ''
+gesto = False
 
 hands_generator = HandsGenerator()
 hands_generator.create(ctx)
@@ -39,6 +40,8 @@ hands_generator.create(ctx)
 ctx.start_generating_all()
 
 def gesture_detected(src, gesture, id, end_point):
+    global gesto
+    gesto = True
     hands_generator.start_tracking(end_point)
 
 def gesture_progress(src, gesture, point, progress): pass
@@ -56,13 +59,16 @@ def update(src, id, pos, time):
     coordenada_projecao = ', '.join(str(e) for e in centro)
 
 def destroy(src, id, time):
-    return
+    global gesto
+    gesto = False
 
 gesture_generator.register_gesture_cb(gesture_detected, gesture_progress)
 hands_generator.register_hand_cb(create, update, destroy)
 
 def processa_frame(imagem):
     cv.SetData(imagem_cv, imagem)
+    if gesto == False:
+      cv.PutText(imagem_cv, 'Acene para ser Rastreado!', (80,50) ,fonte_do_texto , cv.CV_RGB(0,0,0))
     cv.Circle(imagem_cv, centro, 16, cv.CV_RGB(0, 0, 255), 2, cv.CV_AA, 0)
     cv.PutText(imagem_cv, 'Real(mm): '+coordenada_real, (80,435) ,fonte_do_texto , cv.CV_RGB(255,255,255))
     cv.PutText(imagem_cv, 'Convertido(px): '+coordenada_projecao, (80,465) ,fonte_do_texto , cv.CV_RGB(255,255,255))
