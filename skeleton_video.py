1c458fc3a0553831440e40fa9a2c2bbb263450a6
Élisson Michael <elissonmichael@yahoo.com.br>
2013-06-23 18:12:22 -0300

    Experimentando desempenho com a função em C

--- skeleton_video.py
+++ skeleton_video.py
@@ -3,6 +3,7 @@
 from openni import *
 from cv2 import *
 import numpy as np
+from scipy import weave
 
 joints = (SKEL_HEAD, SKEL_LEFT_ELBOW, SKEL_LEFT_FOOT, SKEL_LEFT_HAND, SKEL_LEFT_HIP, SKEL_LEFT_KNEE, SKEL_LEFT_SHOULDER, SKEL_NECK, SKEL_RIGHT_ELBOW, SKEL_RIGHT_FOOT, SKEL_RIGHT_HAND, SKEL_RIGHT_HIP, SKEL_RIGHT_KNEE, SKEL_RIGHT_SHOULDER, SKEL_TORSO)
 # SKEL_LEFT_ANKLE, SKEL_LEFT_COLLAR, SKEL_LEFT_FINGERTIP, SKEL_LEFT_WRIST, SKEL_RIGHT_ANKLE, SKEL_RIGHT_COLLAR, SKEL_RIGHT_FINGERTIP, SKEL_RIGHT_WRIST, SKEL_WAIST
@@ -67,6 +68,25 @@ skel_cap.set_profile(SKEL_PROFILE_ALL)
 # Start generating
 ctx.start_generating_all()
 
+def c_tuple_to_image(tupla, array):
+    assert(type(tupla) == type(()))
+    assert(type(array) == type(np.array([])))
+
+    codigo_em_c = """
+            int i, j;
+            for (i = 0; i < 480; i++){
+              for (j = 0; j < 640; j++){
+                if (tupla[j + i*640] == 1){
+                  array[j + i*640] = 255;
+                }
+                else{
+                  array[j + i*640] = 0;
+                }
+              }
+            }
+           """
+    return weave.inline(codigo_em_c, ['tupla', 'array'])
+
 tecla = -1
 while (tecla < 0):
     ctx.wait_one_update_all(depth)
@@ -75,21 +95,22 @@ while (tecla < 0):
 
     if user.users != []:
       id = user.users[0]
-      #contorno = user.get_user_pixels(id)
+      contorno = user.get_user_pixels(id)
+      c_tuple_to_image(contorno, imagem_binaria)
       #for i in xrange(480):
       #  for j in xrange(640):
       #    if contorno[j + i*640] == 1:
       #      imagem_binaria[i,j] = 255
       #    else:
       #      imagem_binaria[i,j] = 0
-      points = []
-      if skel_cap.is_tracking(id):
-          for joint in joints:
-            points.append(skel_cap.get_joint_position(id, joint).point)
+      #points = []
+      #if skel_cap.is_tracking(id):
+      #    for joint in joints:
+      #      points.append(skel_cap.get_joint_position(id, joint).point)
 
-      points = depth.to_projective(points)
-      for point in points:
-        cv.Circle(imagem_cv, (int(point[0]),int(point[1])), 6, cv.CV_RGB(0, 0, 100), -1, cv.CV_AA, 0)
+      #points = depth.to_projective(points)
+      #for point in points:
+        #cv.Circle(imagem_cv, (int(point[0]),int(point[1])), 4, cv.CV_RGB(0, 0, 100), -1, cv.CV_AA, 0)
 
     cv.ShowImage('Video', imagem_cv)
 
