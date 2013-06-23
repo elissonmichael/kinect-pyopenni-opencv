85f29ea9213400c883dc1c4aaaec2b71b1f96a33
Élisson Michael <elissonmichael@yahoo.com.br>
2013-06-23 18:04:00 -0300

    Consegui Transformar a tupla no array em C com weave

--- weave.py
+++ weave.py
@@ -8,12 +8,14 @@ def c_tuple_to_image(tupla, array):
     assert(type(array) == type(np.array([])))
 
     codigo_em_c = """
-            int max = tupla.length();
             int i, j;
-            for (i = 0; i <= 2; i++){
-              for (j = 0; j <= 2; j++){
+            for (i = 0; i < 3; i++){
+              for (j = 0; j < 3; j++){
                 if (tupla[j + i*3] == 1){
-                  printf ("Epa ");
+                  array[j + i*3] = 255;
+                }
+                else{
+                  array[j + i*3] = 0;
                 }
               }
             }
@@ -22,14 +24,13 @@ def c_tuple_to_image(tupla, array):
 
 
 t = (0,0,0,1,1,1,0,0,0)
-n = np.array([])
+n = np.array([[0,0,0],[0,0,0],[0,0,0]])
 
 c_tuple_to_image(t,n)
 
+print t
+print n
 
-#for i in xrange(3):
-#  for j in xrange(3):
-#    print j + i*3
 
 
 
