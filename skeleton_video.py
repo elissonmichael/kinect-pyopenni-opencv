3a6d77c62921df642cd25e20b5ab93e327b1177d
Élisson Michael <elissonmichael@yahoo.com.br>
2013-06-23 19:32:09 -0300

    Desenhei o Esqueleto e o Contorno

--- skeleton_video.py
+++ skeleton_video.py
@@ -5,20 +5,26 @@ from cv2 import *
 import numpy as np
 from scipy import weave
 
-joints = (SKEL_HEAD, SKEL_LEFT_ELBOW, SKEL_LEFT_FOOT, SKEL_LEFT_HAND, SKEL_LEFT_HIP, SKEL_LEFT_KNEE, SKEL_LEFT_SHOULDER, SKEL_NECK, SKEL_RIGHT_ELBOW, SKEL_RIGHT_FOOT, SKEL_RIGHT_HAND, SKEL_RIGHT_HIP, SKEL_RIGHT_KNEE, SKEL_RIGHT_SHOULDER, SKEL_TORSO)
+membros = {'Tronco' : (SKEL_HEAD, SKEL_NECK, SKEL_TORSO), 'Bracos' : (SKEL_LEFT_HAND, SKEL_LEFT_ELBOW, SKEL_LEFT_SHOULDER, SKEL_RIGHT_SHOULDER, SKEL_RIGHT_ELBOW, SKEL_RIGHT_HAND), 'Perna Direita' : (SKEL_RIGHT_HIP, SKEL_RIGHT_KNEE, SKEL_RIGHT_FOOT), 'Perna Esquerda' : (SKEL_LEFT_HIP, SKEL_LEFT_KNEE, SKEL_LEFT_FOOT)}
+
+# Pontos que não funcionaram
 # SKEL_LEFT_ANKLE, SKEL_LEFT_COLLAR, SKEL_LEFT_FINGERTIP, SKEL_LEFT_WRIST, SKEL_RIGHT_ANKLE, SKEL_RIGHT_COLLAR, SKEL_RIGHT_FINGERTIP, SKEL_RIGHT_WRIST, SKEL_WAIST
 
 pose_to_use = 'Psi'
 
 imagem_cv = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
+branca = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
+cv.Set(branca, (255,255,255))
 imagem_binaria = np.zeros((480,640))
 
 fonte_do_texto = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 0.7, 0.7, 0, 1, 4)
-
 cv.NamedWindow('Video',1)
-cv.MoveWindow('Video',0,0)
 cv.NamedWindow('Binária',1)
-cv.MoveWindow('Binária',720,0)
+cv.NamedWindow('Esqueleto',1)
+
+cv.MoveWindow('Video',0,0)
+cv.MoveWindow('Binária',640,0)
+cv.MoveWindow('Esqueleto',1280,0)
 
 ctx = Context()
 ctx.init()
@@ -34,27 +40,29 @@ skel_cap = user.skeleton_cap
 pose_cap = user.pose_detection_cap
 
 def new_user(src, id):
-    print "1/4 User {} detected. Looking for pose..." .format(id)
+    #print "1/4 User {} detected. Looking for pose..." .format(id)
     pose_cap.start_detection(pose_to_use, id)
 
 def pose_detected(src, pose, id):
-    print "2/4 Detected pose {} on user {}. Requesting calibration..." .format(pose,id)
+    #print "2/4 Detected pose {} on user {}. Requesting calibration..." .format(pose,id)
     pose_cap.stop_detection(id)
     skel_cap.request_calibration(id, True)
 
 def calibration_start(src, id):
-    print "3/4 Calibration started for user {}." .format(id)
+    #print "3/4 Calibration started for user {}." .format(id)
+    pass
 
 def calibration_complete(src, id, status):
     if status == CALIBRATION_STATUS_OK:
-        print "4/4 User {} calibrated successfully! Starting to track." .format(id)
+        #print "4/4 User {} calibrated successfully! Starting to track." .format(id)
         skel_cap.start_tracking(id)
     else:
-        print "ERR User {} failed to calibrate. Restarting process." .format(id)
+        #print "ERR User {} failed to calibrate. Restarting process." .format(id)
         new_user(user, id)
 
 def lost_user(src, id):
-    print "--- User {} lost." .format(id)
+    pass
+    #print "--- User {} lost." .format(id)
 
 # Register them
 user.register_user_cb(new_user, lost_user)
@@ -92,6 +100,7 @@ while (tecla < 0):
     ctx.wait_one_update_all(depth)
     imagem = video.get_raw_image_map_bgr()
     cv.SetData(imagem_cv, imagem)
+    esqueleto = cv.CloneImage(branca)
 
     if user.users != []:
       id = user.users[0]
@@ -103,17 +112,22 @@ while (tecla < 0):
       #      imagem_binaria[i,j] = 255
       #    else:
       #      imagem_binaria[i,j] = 0
-      #points = []
-      #if skel_cap.is_tracking(id):
-      #    for joint in joints:
-      #      points.append(skel_cap.get_joint_position(id, joint).point)
+      points = {}
+      if skel_cap.is_tracking(id):
+          for membro in membros:
+            points[membro] = []
+            for junta in membros[membro]:
+              points[membro].append(skel_cap.get_joint_position(id, junta).point)
 
-      #points = depth.to_projective(points)
-      #for point in points:
-        #cv.Circle(imagem_cv, (int(point[0]),int(point[1])), 4, cv.CV_RGB(0, 0, 100), -1, cv.CV_AA, 0)
+          for membro in points:
+            points[membro] = depth.to_projective(points[membro])
+            for i in xrange(len(points[membro])):
+              cv.Circle(esqueleto, (int(points[membro][i][0]),int(points[membro][i][1])), 6, cv.CV_RGB(0, 0, 100), -1, cv.CV_AA, 0)
+              if i+1 < len(points[membro]):
+                cv.Line(esqueleto, (int(points[membro][i][0]),int(points[membro][i][1])), (int(points[membro][i+1][0]),int(points[membro][i+1][1])), cv.CV_RGB(0,0,0), 1, cv.CV_AA, 0) 
 
     cv.ShowImage('Video', imagem_cv)
-
+    cv.ShowImage('Esqueleto', esqueleto)
     imshow('Binária',imagem_binaria)
     tecla = cv.WaitKey(1)
 cv.DestroyAllWindows()
