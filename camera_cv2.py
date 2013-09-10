8a6852000d95ff7f9a903f9aa783c814cff2b5e3
Élisson Michael <elissonmichael@yahoo.com.br>
2013-09-10 11:34:06 -0300

    Exibir Imagem em Tons de Cinza também

--- camera_cv2.py
+++ camera_cv2.py
@@ -5,6 +5,8 @@ camera = cv2.VideoCapture(0)
 tecla = -1
 while (tecla < 0):
     _,imagem = camera.read()
-    cv2.imshow('camera',imagem)
+    cv2.imshow('camera', imagem)
+    cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('cinza', cinza)
     tecla = cv2.waitKey(1)
 cv2.destroyAllWindows()
