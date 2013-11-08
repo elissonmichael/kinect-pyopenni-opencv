3a7c83cfa3cfecd3ce7cc383b7354665a7439e68
Élisson Michael <elissonmichael@yahoo.com.br>
2013-11-08 16:05:06 -0200

    Refactoring PlayBack com CV2

--- cv2_playback.py
+++ cv2_playback.py
@@ -7,16 +7,15 @@ ni = Context()
 ni.init()
 ni.open_file_recording("MeuVideo.oni")
 ni.start_generating_all()
-video = ni.find_existing_node(NODE_TYPE_IMAGE)
+depth = ni.find_existing_node(NODE_TYPE_DEPTH)
 
-tecla = -1
-while (tecla < 0):
-    ni.wait_one_update_all(video)
-    imagem = video.get_raw_image_map_bgr()
-    imagem_cv = numpy.fromstring(imagem, dtype=numpy.uint8).reshape(480, 640, 3)
-    cv2.imshow("video", imagem_cv)
+while True:
+    ni.wait_one_update_all(depth)
+    imagem = depth.get_raw_depth_map_8()
+    imagem_cv = numpy.fromstring(imagem, dtype=numpy.uint8).reshape(480, 640, 1)
+    cv2.imshow("Profundidade", imagem_cv)
 
-    tecla = cv2.waitKey(1)
+    cv2.waitKey(1)
 cv.DestroyAllWindows()
 
 
