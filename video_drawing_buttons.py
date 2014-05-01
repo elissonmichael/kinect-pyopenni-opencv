d4ec69ce2366e68149cd90f65196f78e18ee51ef
Élisson Michael <elissonmichael@yahoo.com.br>
2014-05-01 11:03:44 -0300

    Moving Main to Bottom + 100% Translated

--- video_drawing_buttons.py
+++ video_drawing_buttons.py
@@ -1,40 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
 from openni import *
 import cv2.cv as cv
-
-cv.NamedWindow('Video',1)
-cv.MoveWindow('Video',0,0)
-cv.NamedWindow('Drawing',1)
-cv.MoveWindow('Drawing',720,0)
-
-fonte_do_texto = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 0.6, 0.6, 0, 1, 4)
-
-quadro = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
-cv.Set(quadro, (255.0,255.0,255.0))
-
-imagem_cv = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
-hands = {}
-buttons_size = (100, 60)
-buttons = {'White': {'color': cv.CV_RGB(255,255,255), 'start': (200, 230), 'end': (200 + buttons_size[0], 230 + buttons_size[1])},
-          'Black': {'color': cv.CV_RGB(0,0,0), 'start': (400, 230), 'end': (400 + buttons_size[0], 230 + buttons_size[1])}
-          }
-
-ni = Context()
-ni.init()
-ni.open_file_recording("paola.oni")
-video = ni.find_existing_node(NODE_TYPE_IMAGE)
-depth = ni.find_existing_node(NODE_TYPE_DEPTH)
-
-gesture_generator = GestureGenerator()
-gesture_generator.create(ni)
-gesture_generator.add_gesture('Wave')
-gesture_generator.add_gesture('Click')
-
-hands_generator = HandsGenerator()
-hands_generator.create(ni)
-
-ni.start_generating_all()
 
 def translate_coordinates(openni_point):
   opencv_point = depth.to_projective([openni_point])
@@ -70,55 +35,90 @@ def create(src, id, pos, time):
     global hands
     ponto = depth.to_projective([pos])
     centro = (int(ponto[0][0]), int(ponto[0][1])) 
-    hands[id] = {'atual': centro, 'drawing': False, 'color': {'name': 'Choose a Color', 'cv': cv.CV_RGB(255,255,255)}}
+    hands[id] = {'current_position': centro, 'drawing': False, 'color': {'name': 'Choose a Color', 'cv': cv.CV_RGB(255,255,255)}}
 
 def update(src, id, pos, time):
     global hands
-    hands[id]['anterior'] = hands[id]['atual']
-    hands[id]['atual'] = translate_coordinates(pos)
+    hands[id]['previous_position'] = hands[id]['current_position']
+    hands[id]['current_position'] = translate_coordinates(pos)
 
 def destroy(src, id, time):
     global hands
     del hands[id]
 
+def update_video_with(image):
+    cv.SetData(cv_image, image)
+    if hands:
+      if hands[1]['drawing']:
+        update_notification_with('Click to Stop Drawing')
+      else:
+        update_notification_with('Click to Start Drawing')
+
+      for id in hands:
+        cv.PutText(cv_image, hands[id]['color']['name'], hands[id]['current_position'] ,text_font , cv.CV_RGB(255,255,255))
+    else:
+      update_notification_with('Wave to Interact')
+    for button in buttons:
+      cv.Rectangle(cv_image, buttons[button]['start'], buttons[button]['end'] , buttons[button]['color'], -1, cv.CV_AA, 0)
+    cv.ShowImage('Video', cv_image)
+
+def update_notification_with(text):
+    cv.PutText(cv_image, text, (240,30) ,text_font , cv.CV_RGB(255,255,255))
+
+def update_drawing():
+    blink = cv.CloneImage(drawing)
+    if hands:
+      for id in hands:
+        cv.Circle(blink, hands[id]['current_position'], 10, hands[id]['color']['cv'], -1, cv.CV_AA, 0)
+        if hands[id]['drawing'] == True:
+          cv.Line(drawing, hands[id]['previous_position'], hands[id]['current_position'], hands[id]['color']['cv'], 10, cv.CV_AA, 0) 
+    cv.ShowImage('Drawing', blink)
+
+########################### MAIN ##################################
+
+cv.NamedWindow('Video',1)
+cv.MoveWindow('Video',0,0)
+cv.NamedWindow('Drawing',1)
+cv.MoveWindow('Drawing',720,0)
+
+text_font = cv.InitFont(cv.CV_FONT_HERSHEY_SIMPLEX, 0.6, 0.6, 0, 1, 4)
+
+drawing = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
+cv.Set(drawing, (255.0,255.0,255.0))
+
+cv_image = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
+hands = {}
+buttons_size = (100, 60)
+buttons = {'White': {'color': cv.CV_RGB(255,255,255), 'start': (200, 230), 'end': (200 + buttons_size[0], 230 + buttons_size[1])},
+          'Black': {'color': cv.CV_RGB(0,0,0), 'start': (400, 230), 'end': (400 + buttons_size[0], 230 + buttons_size[1])}
+          }
+
+ni = Context()
+ni.init()
+ni.open_file_recording("paola.oni")
+video = ni.find_existing_node(NODE_TYPE_IMAGE)
+depth = ni.find_existing_node(NODE_TYPE_DEPTH)
+
+gesture_generator = GestureGenerator()
+gesture_generator.create(ni)
+gesture_generator.add_gesture('Wave')
+gesture_generator.add_gesture('Click')
+
+hands_generator = HandsGenerator()
+hands_generator.create(ni)
+
+ni.start_generating_all()
+
 gesture_generator.register_gesture_cb(gesture_detected, gesture_progress)
 hands_generator.register_hand_cb(create, update, destroy)
 
-def processa_frame(imagem):
-    cv.SetData(imagem_cv, imagem)
-    if hands:
-      if hands[1]['drawing']:
-        update_notification('Click to Stop Drawing')
-      else:
-        update_notification('Click to Start Drawing')
-
-      for id in hands:
-        cv.PutText(imagem_cv, hands[id]['color']['name'], hands[id]['atual'] ,fonte_do_texto , cv.CV_RGB(255,255,255))
-    else:
-      update_notification('Wave to Interact')
-    for button in buttons:
-      cv.Rectangle(imagem_cv, buttons[button]['start'], buttons[button]['end'] , buttons[button]['color'], -1, cv.CV_AA, 0)
-    cv.ShowImage('Video', imagem_cv)
-
-def update_notification(text):
-    cv.PutText(imagem_cv, text, (240,30) ,fonte_do_texto , cv.CV_RGB(255,255,255))
-
-def altera_quadro():
-    blink = cv.CloneImage(quadro)
-    if hands:
-      for id in hands:
-        cv.Circle(blink, hands[id]['atual'], 10, hands[id]['color']['cv'], -1, cv.CV_AA, 0)
-        if hands[id]['drawing'] == True:
-          cv.Line(quadro, hands[id]['anterior'], hands[id]['atual'], hands[id]['color']['cv'], 10, cv.CV_AA, 0) 
-    cv.ShowImage('Drawing', blink)
-
-tecla = -1
-while (tecla < 0):
+key = -1
+while (key < 0):
     ni.wait_any_update_all()
-    imagem = video.get_raw_image_map_bgr()
-    processa_frame(imagem)
-    altera_quadro()
+    image = video.get_raw_image_map_bgr()
+    update_video_with(image)
+    update_drawing()
+    key = cv.WaitKey(1)
 
-    tecla = cv.WaitKey(1)
 cv.DestroyAllWindows()
-cv.SaveImage("Drawing.jpg", quadro)
+cv.SaveImage("Drawing.jpg", drawing)
